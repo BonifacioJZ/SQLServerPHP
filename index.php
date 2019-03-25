261af0c9815dfd08588cf35e1f471e6f91998a33
Bonifacio Juzrez Ceja <revanjz6@gmail.com>
2019-03-25 00:51:16 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php?
+$server = "192.168.0.52";
+$connectionifo = array("Database"=>"datos","UID"=>"tics","PWS"=>"tic2019");
+$conn =  sqlsrv_connect($server,$connectionifo);
+if($conn){
+    echo "Conexión establecida.<br />";
+}else{
+    echo "Conexión no se pudo establecer.<br />";
+    die( print_r( sqlsrv_errors(), true));
+}
